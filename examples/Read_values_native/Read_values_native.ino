ee10154a2367c2c0c68884e705dbf08a8cc1f907
Main Push Robot <robot@example.com>
2022-02-17 06:59:44 +0000

    Committing dev changes

--- examples/Read_values_native/Read_values_native.ino
+++ examples/Read_values_native/Read_values_native.ino
@@ -1,15 +1,15 @@
 /**
  **************************************************
- *
- * @file        Read_values.ino
- * @brief       Example for using the digital and analog read functions for Simple fire sensor
- *
- *
- *	product: www.solde.red/333079
- *
- * @authors     Goran Juric for Soldered.com
+
+   @file        Read_values.ino
+   @brief       Example for using the digital and analog read functions for Simple fire sensor
+
+
+ 	product: www.solde.red/333079
+
+   @authors     Goran Juric for Soldered.com
  ***************************************************/
- 
+
 //Connecting diagram
 //
 //Breakout      Arduino
@@ -29,24 +29,24 @@ SimpleFireSensor sensor(ANALOG_PIN);
 
 void setup()
 {
-    // Initialize the serial communication via UART
-    Serial.begin(115200);
+  // Initialize the serial communication via UART
+  Serial.begin(115200);
 
-    // Initialize the sensor
-    sensor.begin();
+  // Initialize the sensor
+  sensor.begin();
 }
 
 void loop()
 {
-    
+
 
   Serial.print("IR light sensor reading: "); 	// Print information message
   Serial.println(sensor.getValue());  			// Prints raw value of fire sensor
 
   if (digitalRead(DIGITAL_PIN))					//Potentiometer on breakout board is used to
-												//set treshold value. This function checks if
-												//treshold value is passed and determines if there
-												//is a fire nearby.
+    //set treshold value. This function checks if
+    //treshold value is passed and determines if there
+    //is a fire nearby.
   {
     Serial.println("Fire is not detected.");
   }
