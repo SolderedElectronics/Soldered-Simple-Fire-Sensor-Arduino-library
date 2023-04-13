054e098674c640ed2f6b3b3f045c84ab103af510
Main Push Robot <robot@example.com>
2023-04-13 08:11:16 +0000

    Committing dev changes

--- examples/Read_values_easyC/Read_values_easyC.ino
+++ examples/Read_values_easyC/Read_values_easyC.ino
@@ -2,14 +2,26 @@
  **************************************************
 
     @file        Read_values_easyC.ino
-    @brief       Example for using the digital and analog read functions for Simple fire sensor with easyC
+    @brief       Example for using the digital and analog read functions for Simple fire sensor with easyC.
+
+                 For this example, you will need any Dasduino board, a USB-c cable, a Simple fire sensor with easyC, and easyC cable. 
+                 Don't have Dasduino? Get it here: https://soldered.com/categories/dasduino-arduino/dasduino-boards/ 
+                 Don't have easyC cable? Get it here: https://soldered.com/categories/easyc-2/easyc-cables-adapters/
+
+                 This example will show you how to set thresholds for detecting fire, and how to detect fire using
+                 the sensor. Connect the Simple sensor to the Dasduino using easyC cable and upload the code.
+                 Open the Serial Monitor at 115200 baud rate to see the values. You can use a lighter to see what
+                 number you get when there is fire and then enter these values as thresholds. Now is your sensor
+                 ready to detect a fire.
 
 
-  	product: www.solde.red/333077
+    product: www.solde.red/333077
 
-    @authors     Goran Juric for Soldered.com
+    @authors     Goran Juric & Karlo Leksic for Soldered.com
  ***************************************************/
 
+// Include Soldered library for Simple sensor
+// If you don't have a library, get it here: https://github.com/SolderedElectronics/Soldered-Simple-Fire-Sensor-Arduino-library
 #include "Simple-fire-sensor-easyC-SOLDERED.h"
 
 // Declare the sensor object
@@ -17,31 +29,53 @@ SimpleFireSensor sensor;
 
 void setup()
 {
-  // Initialize the serial communication via UART
-  Serial.begin(115200);
+    // Initialize the serial communication via UART
+    Serial.begin(115200);
 
-  // Initialize the sensor
-  sensor.begin();
+    // Initialize the sensor
+    // Start I2C communication on default address (0x30)
+    sensor.begin();
 
-  sensor.setLowerTresh(850); // You should set these two values, this is value when fire is present (you can use lighter at 1m distance)
-  sensor.setUpperTresh(1000); // This is value when no fire is present
+    // If you want another I2C address, enter it in the bracket
+    // You can set another I2C address (0x31 - 0x37) by changing address switches on the breakout
+    // NOTE: You have to restart breakout to apply the address change by unplugging and plugging
+    // the easyC or USB-c from the Dasduino 
+    // sensor.begin(0x31);
+
+    sensor.setRawLowerTresh(850);  // You should set these two values, this is value when fire is present (you can use
+                                   // lighter at 1m distance)
+    sensor.setRawUpperTresh(1000); // This is value when no fire is present
+
+    // You can use percentage values by calling following functions:
+    // sensor.setLowerTresh(85); // 85%
+    // sensor.setUpperTresh(99); // 99%
+
+    // Set threshold for LED on the breakout
+    sensor.setRawThreshold(250); // or
+    // sensor.setThreshold(25.1); // 25.1%
 }
 
 void loop()
 {
 
-  Serial.print("IR light sensor reading: "); // Print information message
-  Serial.println(sensor.getValue());  // Prints percent value of fire sensor
-  
-  //This function checks to which value is closer value that has been read from sensor
-  if (abs(sensor.getValue() - sensor.lowerTresh()) < abs(sensor.getValue() - sensor.upperTresh()))
-  {
-    Serial.println("Fire is detected!!");   //If sensor value is closer to lowerTresh value, fire is present
-  }
+    Serial.print("IR light sensor reading: "); // Print information message
+    Serial.print(sensor.getValue());           // Prints percent value of fire sensor
+    Serial.print("% ");
+    Serial.println(sensor.getRawValue()); // Prints raw value of fire sensor
 
-  else
-  {
-    Serial.println("Fire is not detected.");    //If sensor value is closer to upperTresh value, fire is present
-  }
+    // This function checks to which value is closer value that has been read from sensor
+    if (sensor.getRawValue() >= sensor.getRawLowerTresh() && sensor.getRawValue() <= sensor.getRawUpperTresh())
+    // You can also use percentage values if you want:
+    // if (sensor.getValue() >= sensor.getLowerTresh() && sensor.getValue() <= sensor.getUpperTresh())
+    {
+        Serial.println("Fire is detected!!");
+    }
 
+    else
+    {
+        Serial.println("Fire is not detected.");
+    }
+
+    // Wait a bit before next reading
+    delay(200);
 }
--- examples/Read_values_native/Read_values_native.ino
+++ examples/Read_values_native/Read_values_native.ino
@@ -2,59 +2,69 @@
  **************************************************
 
    @file        Read_values.ino
-   @brief       Example for using the digital and analog read functions for Simple fire sensor
+   @brief       Example for using the digital and analog read functions for Simple fire sensor.
 
+                For this example, you will need any Dasduino board, a USB-c cable, and Simple fire sensor.
+                Don't have Dasduino? Get it here: https://soldered.com/categories/dasduino-arduino/dasduino-boards/ 
 
- 	product: www.solde.red/333079
+                This example will show you how to read values and detect fire using the sensor. 
+                Connect the Simple sensor to the Dasduino and upload the code.
+                Open the Serial Monitor at 115200 baud rate to see the analog values. 
+                You can use a lighter to set the fire threshold. 
+
+  product: www.solde.red/333079
 
    @authors     Goran Juric for Soldered.com
  ***************************************************/
 
-//Connecting diagram
+// Connecting diagram
 //
-//Breakout      Arduino
-//|-------------|
-//D0------------D9
-//A0------------A0
-//GND-----------GND
-//VCC-----------5V
+// Breakout      Arduino
+//  |-------------|
+// D0------------D9
+// A0------------A0
+// GND-----------GND
+// VCC-----------5V
 
 #include "Simple-fire-sensor-easyC-SOLDERED.h"
 
-#define ANALOG_PIN A0       //Pin for analog read
-#define DIGITAL_PIN 9       //Pin for digital read
+#define ANALOG_PIN  A0 // Pin for analog read
+#define DIGITAL_PIN 9  // Pin for digital read
 
 // Declare the sensor object and set pin on which sensor is connected
 SimpleFireSensor sensor(ANALOG_PIN);
 
 void setup()
 {
-  // Initialize the serial communication via UART
-  Serial.begin(115200);
+    // Initialize the serial communication via UART
+    Serial.begin(115200);
 
-  // Initialize the sensor
-  sensor.begin();
+    // Initialize the sensor
+    sensor.begin();
 }
 
 void loop()
 {
+    Serial.print("IR light sensor reading: "); // Print information message
+    Serial.print(sensor.getValue());           // Prints percent value of fire sensor
+    Serial.print("% ");
+    Serial.println(sensor.getRawValue()); // Prints raw value of fire sensor
 
+    if (digitalRead(DIGITAL_PIN)) // Potentiometer on breakout board is used to
+                                  // set treshold value. This function checks if
+                                  // treshold value is passed and determines if there
+                                  // is a fire nearby.
+    {
+        Serial.println("Fire is not detected.");
+    }
 
-  Serial.print("IR light sensor reading: "); 	// Print information message
-  Serial.println(sensor.getValue());  			// Prints raw value of fire sensor
+    else
+    {
+        Serial.println("Fire is detected!!");
+    }
 
-  if (digitalRead(DIGITAL_PIN))					//Potentiometer on breakout board is used to
-    //set treshold value. This function checks if
-    //treshold value is passed and determines if there
-    //is a fire nearby.
-  {
-    Serial.println("Fire is not detected.");
-  }
+    // You can also use thresholds (except threshold for LED) like in Read_values_easyC example to detect fire.
 
-  else
-  {
-    Serial.println("Fire is detected!!");
-  }
-  delay(1000);
-
+    // Wait a bit before next reading
+    delay(200);
 }
