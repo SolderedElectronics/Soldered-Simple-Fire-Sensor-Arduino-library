2b854ff498a5327d2ac1d04b3aa9cef3c8f36ff7
Docs Robot <robot@example.com>
2023-04-18 06:44:38 +0000

    Committing documentation changes

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['getlowertresh_0',['getLowerTresh',['../class_simple_fire_sensor.html#a5fe4e3de14d3e4acfeccf639f4002231',1,'SimpleFireSensor']]],
-  ['getrawlowertresh_1',['getRawLowerTresh',['../class_simple_fire_sensor.html#ae3482cc5b812b4102eb9bb4777bcec1d',1,'SimpleFireSensor']]],
-  ['getrawuppertresh_2',['getRawUpperTresh',['../class_simple_fire_sensor.html#a76b33d6e0a299b0d63e142b587ec8eae',1,'SimpleFireSensor']]],
-  ['getrawvalue_3',['getRawValue',['../class_simple_fire_sensor.html#a1dfe02adc353bf27f2d894b26eeaaa28',1,'SimpleFireSensor']]],
+  ['getrawlowertresh_1',['getRawLowerTresh',['../class_simple_fire_sensor.html#a3b6427abf2833cd4de0e30826611a41e',1,'SimpleFireSensor']]],
+  ['getrawuppertresh_2',['getRawUpperTresh',['../class_simple_fire_sensor.html#ab6e865911ba8ae4c0e557ec8991afc8b',1,'SimpleFireSensor']]],
+  ['getrawvalue_3',['getRawValue',['../class_simple_fire_sensor.html#a1d5c117ed584e35510888567f889d4ae',1,'SimpleFireSensor']]],
   ['getuppertresh_4',['getUpperTresh',['../class_simple_fire_sensor.html#aa50dbd263ad041af58c3adf8d5f81e28',1,'SimpleFireSensor']]],
   ['getvalue_5',['getValue',['../class_simple_fire_sensor.html#aac5cfe4ef3bcf2d081d83a058b0b9bac',1,'SimpleFireSensor']]]
 ];
